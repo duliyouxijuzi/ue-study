41b4de818de56186efff35799d0f47c01f7a757a
Juzi <58835407+duliyouxijuzi@users.noreply.github.com>
2023-12-15 16:00:00 +0800

    Update theme.ts

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -14,7 +14,13 @@ export default hopeTheme({
 
   logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  // 默认为 GitHub. 同时也可以是一个完整的 URL
+  repo: "duliyouxijuzi/ue-study",
+	// 自定义仓库链接文字。默认从 `repo` 中自动推断为
+  // "GitHub" / "GitLab" / "Gitee" / "Bitbucket" 其中之一，或是 "Source"。
+  repoLabel: "GitHub",
+  // 是否在导航栏内显示仓库链接，默认为 `true`
+  repoDisplay: true,
 
   docsDir: "src",
 
